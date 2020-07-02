3a97e357ff2fdb93289648f44f43e18eff7caf97
ShNorah <aksharon018@gmail.com>
2020-07-02 07:50:37 +0300

    Updates

---
+++ precache-manifest.23776c0a3f887f2e08d288846eab60d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96006710c857e61502d06c07c7b00a2",
+    "url": "/exercise-tracker-app/index.html"
+  },
+  {
+    "revision": "b1b410d17bff98c95b21",
+    "url": "/exercise-tracker-app/static/css/2.bf5b17f7.chunk.css"
+  },
+  {
+    "revision": "b1b410d17bff98c95b21",
+    "url": "/exercise-tracker-app/static/js/2.9a2fd78d.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/exercise-tracker-app/static/js/2.9a2fd78d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a18ded27e310e8aad5b2",
+    "url": "/exercise-tracker-app/static/js/main.e336e370.chunk.js"
+  },
+  {
+    "revision": "8bcc46db78e576fe5ad3",
+    "url": "/exercise-tracker-app/static/js/runtime-main.2e2ca745.js"
+  }
+]);
